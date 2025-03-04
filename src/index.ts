4fa38b625e99df17e541a5423500fbd163df7cc0
k-arikawa0312 <s1F102402710@iniad.org>
2025-03-04 12:10:11 +0900

    getとpostapi

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,50 @@
 import { Hono } from 'hono'
+import { prettyJSON } from 'hono/pretty-json'
 
 const app = new Hono()
 
+let blogPosts = [
+  {
+    id: 1,
+    title: 'First Blog Post',
+    content: 'This is my first blog post'
+  },
+  {
+    id: 2,
+    title: 'Second Blog Post',
+    content: 'This is my second blog post'
+  },
+  {
+    id: 3,
+    title: 'Third Blog Post',
+    content: 'This is my third blog post'
+  }
+]
+
+app.use("*", prettyJSON())
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/posts', (c) => c.json({posts: blogPosts}))
+
+app.get('/posts/:id', (c) => {
+  const id = c.req.param("id")
+  const post = blogPosts.find(p => p.id === Number(id))
+
+  if (post) {
+    return c.json(post)
+  } else {
+    return c.status(404).json({error: 'Post not found'})
+  }
+})
+
+app.post('/posts',async (c) =>  {
+  const {title, content} = await c.req.json<{title: string, content: string}>()
+  const newPost= {id: blogPosts.length + 1, title, content}
+  blogPosts = [...blogPosts, newPost]
+  return c.json(newPost)
 })
 
 export default app
